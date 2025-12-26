62f4dddd666a94dd33a9921ce9c68eb43b316dea
vermeil <13688974125@163.com>
2025-12-27 03:10:17 +0800

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -111,9 +111,10 @@ fi
 /etc/init.d/cron restart
 echo -e "${Info}自我唤醒的定时任务添加成功！！"
 
+github='https://github.com/Lintao-Zeng/GoogleCloudShell/raw/refs/heads/main'
 echo -e "\n${Info}如果您之前在 $(green_font 'https://ssh.cloud.google.com') 执行过此脚本"
 echo -e "${Info}那么以后再执行此脚本只需运行 $(red_font './gcs.sh') 即可，即使机器重置也不受影响"
-echo -e "${Tip}在其它机器定时唤醒此Shell：$(green_font 'wget -O gcs_k.sh '${github}'/gcs/gcs_k.sh && chmod +x gcs_k.sh && ./gcs_k.sh')"
+echo -e "${Tip}在其它机器定时唤醒此Shell：$(green_font 'wget -O gcs_k.sh '${github}'/gcs_k.sh && chmod +x gcs_k.sh && ./gcs_k.sh')"
 
 install_v2ray(){
 	$PM -y install jq curl lsof
