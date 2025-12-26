dadc71f1a7836f1f98632f08985677ef297f7dcf
vermeil <13688974125@163.com>
2025-12-27 02:46:57 +0800

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -3,14 +3,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 stty erase ^H
 
-sh_ver='1.4.2'
-github='https://github.com/Lintao-Zeng/GoogleCloudShell/raw/refs/heads/main'
-new_ver=$(curl -s "${github}"/gcs.sh|grep 'sh_ver='|head -1|awk -F '=' '{print $2}'|sed $'s/\'//g')
-if [[ $sh_ver != "${new_ver}" ]]; then
-	wget -qO gcs.sh ${github}/gcs.sh
-	exec ./gcs.sh
-fi
-
 green_font(){
 	echo -e "\033[32m\033[01m$1\033[0m\033[37m\033[01m$2\033[0m"
 }
